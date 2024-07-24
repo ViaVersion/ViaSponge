7c63a7f51024e4c9b9ef85824c4bba8467f33e84
Nassim Jahnke <nassim@njahnke.dev>
2024-07-24 13:40:24 +0200

    1.0.1 Release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,19 @@
 plugins {
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.github.goooler.shadow") version "8.1.8"
     id("java")
 }
 
 group = "com.viaversion"
-version = "1.0.0"
+version = "1.0.1"
 
 dependencies {
     compileOnly("com.viaversion:viaversion-common:5.0.2-SNAPSHOT")
-    compileOnly("com.viaversion:viabackwards-common:5.0.0-SNAPSHOT")
-    compileOnly("com.viaversion:viarewind-common:4.0.0-SNAPSHOT")
+    compileOnly("com.viaversion:viabackwards-common:5.0.1")
+    compileOnly("com.viaversion:viarewind-common:4.0.1")
     compileOnly("net.raphimc:viaaprilfools-common:3.0.1-SNAPSHOT")
     compileOnly("io.netty:netty-all:4.0.20.Final")
     compileOnly("org.spongepowered:spongeapi:8.0.0")
-    implementation("net.lenni0451:Reflect:1.3.2")
+    implementation("net.lenni0451:Reflect:1.3.4")
 }
 
 tasks {
