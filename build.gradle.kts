9ecbe22bc66a31c1e13e6bc4abdf2138f99a0eb2
Nassim Jahnke <nassim@njahnke.dev>
2024-08-08 11:08:34 +0200

    Update shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("io.github.goooler.shadow") version "8.1.8"
+    id("com.gradleup.shadow") version "8.3.0"
     id("java")
 }
 
@@ -7,10 +7,10 @@ group = "com.viaversion"
 version = "1.0.1"
 
 dependencies {
-    compileOnly("com.viaversion:viaversion-common:5.0.2-SNAPSHOT")
-    compileOnly("com.viaversion:viabackwards-common:5.0.1")
-    compileOnly("com.viaversion:viarewind-common:4.0.1")
-    compileOnly("net.raphimc:viaaprilfools-common:3.0.1-SNAPSHOT")
+    compileOnly("com.viaversion:viaversion-common:5.0.3")
+    compileOnly("com.viaversion:viabackwards-common:5.0.3")
+    compileOnly("com.viaversion:viarewind-common:4.0.2")
+    compileOnly("net.raphimc:viaaprilfools-common:3.0.1")
     compileOnly("io.netty:netty-all:4.0.20.Final")
     compileOnly("org.spongepowered:spongeapi:8.0.0")
     implementation("net.lenni0451:Reflect:1.3.4")
