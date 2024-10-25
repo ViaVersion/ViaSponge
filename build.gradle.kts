9ea85e1a0a0184cd807ffae5c51eb2a8805e9358
FlorianMichael <florian.michael07@gmail.com>
2024-10-25 18:31:23 +0200

    Call ViaBackwardsPlatform#enable

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ group = "com.viaversion"
 version = "1.1.0"
 
 dependencies {
-    compileOnly("com.viaversion:viaversion-common:5.0.3")
-    compileOnly("com.viaversion:viabackwards-common:5.0.3")
+    compileOnly("com.viaversion:viaversion-common:5.1.0")
+    compileOnly("com.viaversion:viabackwards-common:5.1.0")
     compileOnly("com.viaversion:viarewind-common:4.0.2")
     compileOnly("net.raphimc:viaaprilfools-common:3.0.1")
     compileOnly("io.netty:netty-all:4.1.112.Final")
